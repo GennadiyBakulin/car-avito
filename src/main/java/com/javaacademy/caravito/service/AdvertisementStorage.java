5fb3a623065ffddc3afc15224cd1651ae3a2e4b7
Gennadiy Bakulin <g.bakulin@yandex.ru>
2024-12-01 11:16:24 +0700

    Этап 3 Создал сервис хранения объявлений

---
+++ src/main/java/com/javaacademy/caravito/service/AdvertisementStorage.java
@@ -0,0 +1,32 @@
+package com.javaacademy.caravito.service;
+
+import com.javaacademy.caravito.advertisement.Advertisement;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AdvertisementStorage {
+
+  private final Map<Integer, Advertisement> advertisementStorage = new HashMap<>();
+  private Integer count = 0;
+
+  public void save(Advertisement advertisement) {
+    advertisement.setId(++count);
+    advertisementStorage.put(count, advertisement);
+  }
+
+  public List<Advertisement> getAllAdvertisement() {
+    return advertisementStorage.values().stream().toList();
+  }
+
+  public Optional<Advertisement> getAdvertisementById(Integer id) {
+    return Optional.ofNullable(advertisementStorage.get(id));
+  }
+
+  public boolean deleteById(Integer id) {
+    return advertisementStorage.remove(id) != null;
+  }
+}
